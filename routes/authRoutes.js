936c0536ae52c8f978191810dd0054f4b8b640e6
Tashipelden11 <tashipelden@gmail.com>
2025-03-20 18:46:53 +0500

    Practical 1

---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router(); // Fixed typo here
+const authController = require('../controllers/authController');
+
+router.post('/signup', authController.registerUser);
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.post('/login', authController.loginUser);
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/logout', authController.logoutUser);
+
+module.exports = router;
